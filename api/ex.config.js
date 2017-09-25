6cb123459e50cc6961c19b0e9b89c93a979546ae
jicjjang <jicjjang12@gmail.com>
2017-09-25 22:55:39 +0900

    append config

--- api/ex.config.js
+++ api/ex.config.js
@@ -1,3 +1,4 @@
 export const API_KEY = 'API_KEY'
-export const DB_URL = 'mongodb://127.0.0.1:27017'
+export const DB_URL = 'mongodb://127.0.0.1:27017/DB_URL'
+export const SESSION_DB_URL = 'mongodb://localhost:27017/SESSION_DB_URL'
 export const SESSION_SECRET = 'SESSION_SECRET_KEY'
