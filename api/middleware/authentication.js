7a001ddc5b5f3a7b2cc960d0fae57178b69f31d6
jicjjang <jicjjang12@gmail.com>
2017-10-03 01:02:48 +0900

    changed the authentication process and removed user regist func

--- api/middleware/authentication.js
+++ api/middleware/authentication.js
@@ -8,8 +8,7 @@ import User from '../models/user'
 export function checkAuth (req, res, next) {
   const keyList = Object.keys(req.query)
 
-  if (keyList.length > 0 && keyList.includes('token') && keyList.includes('id')
-    && keyList.includes('platform')) {
+  if (keyList.length > 0 && keyList.includes('token') && keyList.includes('platform')) {
     switch(req.query.platform) {
       case 'facebook':
         return _commonAuth(next,
@@ -45,11 +44,19 @@ function _commonAuth(next, url, query, includeToken, headers) {
     {} : options.headers = headers
 
   return axios.get(mergedUrl, options).then(res => {
-    if ((query.platform === 'facebook' && res.data.email == query.id)
-      || (query.platform === 'google' && res.data.email == query.id)
-      || (query.platform === 'kakaotalk' && res.data.id == query.id)) {
+    const picture = query.platform === 'facebook' ? res.data.picture.data.url
+      : (query.platform === 'google' ? res.data.picture : res.data.picture)
+    return User.findOneAndUpdate(
+      { is_ok: 1, platform: query.platform, id: res.data.id },
+      { picture }
+    ).exec((err, user) => {
+      if (err || !user) {
+        let errDetail = new Error('You didn\'t have authentication.')
+        errDetail.status = 401
+        return next(errDetail)
+      }
       return next()
-    }
+    })
     throw 'You didn\'t have authentication.'
   }).catch(err => {
     let errDetail = new Error('You didn\'t have authentication.')
@@ -60,39 +67,40 @@ function _commonAuth(next, url, query, includeToken, headers) {
 
 /**
   TODO: DB에 등록된 유저인지 체크하고, 안되어있으면 추가
+  NOTE: **DEPLICATED** 서버에서 저장할 일이 아님.
 **/
-export function checkRegister (req, res, next) {
-  let errDetail = new Error('Database failure.')
-  errDetail.status = 500
-
-  if (!req.query || !req.query.id || !req.query.platform) {
-    let errDetail = new Error('You didn\'t have authentication.')
-    errDetail.status = 401
-    return next(errDetail)
-  }
-
-  User.findOne({'id': req.query.id, 'platform': req.query.platform})
-    .exec((err, user) => {
-    if (err) {
-      return next(errDetail)
-    }
-
-    if (!user) {
-      let newUser = new User()
-      newUser.id = req.query.id
-      newUser.platform = req.query.platform
-      newUser.is_ok = 1
-      newUser.crt_dt = new Date()
-      newUser.udt_dt = newUser.crt_dt
-      newUser.save((err, user) => {
-        if (err) {
-          return next(errDetail)
-        }
-      })
-    }
-  }).then(save => {
-    return next()
-  }).catch(e => {
-    return next(errDetail)
-  })
-}
+// export function checkRegister (req, res, next) {
+//   let errDetail = new Error('Database failure.')
+//   errDetail.status = 500
+//
+//   if (!req.query || !req.query.id || !req.query.platform) {
+//     let errDetail = new Error('You didn\'t have authentication.')
+//     errDetail.status = 401
+//     return next(errDetail)
+//   }
+//
+//   User.findOne({'id': req.query.id, 'platform': req.query.platform})
+//     .exec((err, user) => {
+//     if (err) {
+//       return next(errDetail)
+//     }
+//
+//     if (!user) {
+//       let newUser = new User()
+//       newUser.id = req.query.id
+//       newUser.platform = req.query.platform
+//       newUser.is_ok = 1
+//       newUser.crt_dt = new Date()
+//       newUser.udt_dt = newUser.crt_dt
+//       newUser.save((err, user) => {
+//         if (err) {
+//           return next(errDetail)
+//         }
+//       })
+//     }
+//   }).then(save => {
+//     return next()
+//   }).catch(e => {
+//     return next(errDetail)
+//   })
+// }
