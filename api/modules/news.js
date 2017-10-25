6489cae306fbd264d1a78bf6f46b55ded9f3c694
jicjjang <jicjjang12@gmail.com>
2017-10-25 23:59:37 +0900

    append session func

--- api/modules/news.js
+++ api/modules/news.js
@@ -2,11 +2,16 @@ import News from '../models/news'
 import Brewery from '../models/brewery'
 
 export function getNewsList (location) {
-  return News.find({is_ok: 1}).sort({crt_dt: -1})
-    .exec((err, newsList) => {
-    if (err) {
-      return null
-    }
-    return newsList
-  })
+  if (!req.session.getNewsList) {
+    return News.find({is_ok: 1}).sort({crt_dt: -1})
+      .exec((err, newsList) => {
+      if (err) {
+        return null
+      }
+      req.session.getNewsList = newsList
+      return newsList
+    })
+  } else {
+    return req.session.getNewsList
+  }
 }
