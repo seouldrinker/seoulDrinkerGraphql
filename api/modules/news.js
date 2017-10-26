0a5b46cbb6cea5af631653f7bf73fb9b71966385
jicjjang <jicjjang12@gmail.com>
2017-10-26 19:18:24 +0900

    modified wrong params in news

--- api/modules/news.js
+++ api/modules/news.js
@@ -1,7 +1,7 @@
 import News from '../models/news'
 import Brewery from '../models/brewery'
 
-export function getNewsList (location) {
+export function getNewsList (req) {
   if (!req.session.getNewsList) {
     return News.find({is_ok: 1}).sort({crt_dt: -1})
       .exec((err, newsList) => {
--- api/routes/news.js
+++ api/routes/news.js
@@ -5,7 +5,7 @@ import { getNewsList } from '../modules/news'
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  const results = await getNewsList()
+  const results = await getNewsList(req)
   if (results && typeof results !== 'undefined') {
     return res.send({
       code: 200,
