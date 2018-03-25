61cf5190abe9471f1d6506c7847e72e2e2e5c73f
jicjjang <jicjjang12@gmail.com>
2018-03-25 23:50:05 +0900

    modified project name api to graphql

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 it('Main page content', (done) => {
   axios.get('http://127.0.0.1:3000').then((response) => {
     expect(response.data.split('<body>')[1].split('</body>')[0].trim())
-      .to.equal('Seoul Drinker REST API')
+      .to.equal('Seoul Drinker GraphQL')
     done()
   }).catch(err => {
     done(err)
